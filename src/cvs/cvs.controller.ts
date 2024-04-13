de0af84053ace450262cc129d81c562e851e75a7
Ayadi-Nassim <nassim.ayadi@insat.ucar.tn>
2024-04-13 11:46:46 +0200

    adding uploading cv image

--- src/cvs/cvs.controller.ts
+++ src/cvs/cvs.controller.ts
@@ -11,6 +11,7 @@ import {
   Put,
   Headers,
   UnauthorizedException,
+  BadRequestException,
 } from '@nestjs/common';
 import { CvsService } from './cvs.service';
 import { CreateCvDto } from './dto/create-cv.dto';
@@ -18,6 +19,11 @@ import { UpdateCvDto } from './dto/update-cv.dto';
 import { ExDTO } from './dto/ex.dto';
 
 import { verify } from 'jsonwebtoken';
+
+import { UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
+import * as path from 'path';
 
 @Controller('cvs')
 export class CvsController {
@@ -62,5 +68,47 @@ export class CvsController {
   @Delete(':id')
   async Delete(@Param('id', ParseIntPipe) id) {
     return await this.service.delete(id);
+  }
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('image'))
+  uploadFile(@UploadedFile() file) {
+
+    if (!file) {
+      throw new BadRequestException('No file uploaded');
+    }
+
+    if (!file.originalname) {
+      throw new BadRequestException('Invalid file object');
+    }
+    
+    if (file.size > 1 * 1024 * 1024) {
+      throw new BadRequestException('File size should not exceed 1MB');
+    }
+
+    const allowedMimes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if (!allowedMimes.includes(file.mimetype)) {
+      throw new BadRequestException('Invalid file type. Only JPEG, JPG, and PNG image files are allowed.');
+    }
+    
+    const uploadDirectory = path.join(__dirname, '..','..', 'public', 'uploads');
+    const uploadPath = path.join(uploadDirectory, file.originalname);
+
+    if (!existsSync(uploadDirectory)) {
+      mkdirSync(uploadDirectory, { recursive: true });
+    }
+
+    const writeStream = createWriteStream(uploadPath);
+
+    writeStream.write(file.buffer);
+
+    writeStream.on('finish', () => {
+      console.log('File saved successfully:', uploadPath);
+    });
+
+    writeStream.end();
+    return {
+      filename: file.filename,
+      path: uploadPath,
+    };
   }
 }
